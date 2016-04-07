945c61c2402cb699713ae6b1a77d8efd32e92f43
devonrusconi <drusconi@gcsumb.edu>
2016-04-07 15:42:17 +0000

    Step 3 has ran but getting an error when chosing an argument in --classifier

--- stoqs/contrib/analysis/classify.py
+++ stoqs/contrib/analysis/classify.py
@@ -56,7 +56,8 @@ class Classifier(BiPlot):
     To hold methods and data to support classification of measurements in a STOQS database.
     See http://scikit-learn.org/stable/auto_examples/plot_classifier_comparison.html
     '''
-    classifiers = { 'Nearest_Neighbors': KNeighborsClassifier(3),
+   
+    classifiers = { 'Nearest Neighbors' :  KNeighborsClassifier(3),
                     'Linear_SVM': SVC(kernel="linear", C=0.025),
                     'RBF_SVM': SVC(gamma=2, C=1),
                     'Decision_Tree': DecisionTreeClassifier(max_depth=5),
@@ -245,21 +246,74 @@ class Classifier(BiPlot):
         clf = self.classifiers[self.args.classifier]
 
         X, y = self._loadLabeledData(labeledGroupName)
-
+        datasets = self._loadLabedledData(labledGroupName)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=self.args.test_size, train_size=self.args.train_size)
 
         import pdb
         pdb.set_trace()
         # TODO: Implement graphical evaluation as in http://scikit-learn.org/stable/auto_examples/plot_classifier_comparison.html
+        i = 1
+    # iterate over datasets
+        for ds in datasets:
+        # preprocess dataset, split into training and test part
+            X, y = ds
+            X = StandardScaler().fit_transform(X)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4)
 
-        X_train = StandardScaler().fit_transform(X_train)
+            x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+            y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+            xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        if self.args.verbose:
-            print "  score = %f" % score
+            # just plot the dataset first
+            cm = plt.cm.RdBu
+            cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+            ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    # Plot the training points
+            ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+    # and testing points
+            ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xticks(())
+            ax.set_yticks(())
+            i += 1
 
-        self._saveModel(labeledGroupName, clf)
+    # iterate over classifiers
+        for name, clf in self.classifiers.iteritems():
+            ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+            clf.fit(X_train, y_train)
+            score = clf.score(X_test, y_test)
+
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, m_max]x[y_min, y_max].
+            if hasattr(clf, "decision_function"):
+                Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+            else:
+                Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        # Put the result into a color plot
+            Z = Z.reshape(xx.shape)
+            ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+        # Plot also the training points
+            ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+        # and testing points
+            ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
+                   alpha=0.6)
+
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xticks(())
+            ax.set_yticks(())
+            ax.set_title(name)
+            ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+            i += 1
+     #self._saveModel(labeledGroupName, clf)
+
+    pdb.set_trace()
+    #figure.subplots_adjust(left=.02, right=.98)
+    plt.show()
 
     def _saveModel(self, labeledGroupName, clf):
         '''
